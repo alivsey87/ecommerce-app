83314d083161985decd1ea00232f8b2a370a1a4e
alivsey87 <andre.livsey@gmail.com>
2025-06-11 17:08:57 -0400

    created Login Form

---
+++ src/components/Auth/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { useState, type FormEvent } from "react";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+
+const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Login successful!");
+    } catch (err: unknown) {
+      setError(err instanceof Error ? err.message : "Unknown error occurred.");
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      alert("Logged out!");
+    } catch (err: unknown) {
+      console.error(
+        "Logout error:",
+        err instanceof Error ? err.message : "unknown"
+      );
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <p>{error}</p>}
+      </form>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  );
+};
+
+export default Login;
